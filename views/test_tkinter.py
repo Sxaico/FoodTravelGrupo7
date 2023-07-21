3335a565f7bd58cfe45245b9fd8b97842d0eb1f7
Sxaico <ricardo.nahuel.guzman@gmail.com>
2023-07-20 21:12:21 -0300

    Update test_tkinter.py

--- views/test_tkinter.py
+++ views/test_tkinter.py
@@ -1,9 +1,10 @@
 import tkinter as tk
 
 root = tk.Tk()
-#root.title("Fantastica app FoodTravel")
+root.title("Fantastica app FoodTravel")
 frame = tk.Frame(root).pack()
 
 # Botones
-#boton_inicio = tk.Button(frame, text='Boton inicio').pack()
+boton_inicio = tk.Button(frame, text='Boton inicio').pack()
 
+root.mainloop()
