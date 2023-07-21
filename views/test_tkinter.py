a00c39641db1359bb55578e8bd8d298ef7ec03f0
Sxaico <ricardo.nahuel.guzman@gmail.com>
2023-07-20 21:07:56 -0300

    Avance sobre tkinter

---
+++ views/test_tkinter.py
@@ -0,0 +1,9 @@
+import tkinter as tk
+
+root = tk.Tk()
+#root.title("Fantastica app FoodTravel")
+frame = tk.Frame(root).pack()
+
+# Botones
+#boton_inicio = tk.Button(frame, text='Boton inicio').pack()
+
